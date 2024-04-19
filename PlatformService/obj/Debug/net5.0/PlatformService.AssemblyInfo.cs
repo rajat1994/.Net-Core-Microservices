b32a887ffee6e70fc1afcd75cb9da3fd2577c946
Rajat Arora <rajatarora25@yahoo.in>
2024-04-19 13:17:28 +0530

    Prep Db check in
    Prep DB check in

--- PlatformService/obj/Debug/net5.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net5.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce599f20ca10841995a05fed9040ac378cd30496")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c0f48f1d06d6dbf4694f5fe980decd80a29d79")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
