72621ecf215157a49246221a1fa5541bd93a0c8d
Rajat Arora <rajatarora25@yahoo.in>
2024-04-19 13:37:55 +0530

    Added Automappers for platform service

--- PlatformService/obj/Debug/net5.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net5.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32a887ffee6e70fc1afcd75cb9da3fd2577c946")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8f662d00c5ccb53292b49c2893415079bb45d99")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
