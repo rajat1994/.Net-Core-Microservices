3eaa1f5545e6aef0525e7afb179d842a9bdee225
Rajat Arora <rajatarora25@yahoo.in>
2024-04-24 00:12:59 +0530

    Command Service set up

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+namespace CommandService.Controllers
+
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Testing Inbound Connection");
+            return Ok("TestInboundConnection");
+        }
+
+    }
+
+}
